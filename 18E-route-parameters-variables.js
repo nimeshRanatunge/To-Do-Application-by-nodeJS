218a5771f105711daa8a839ba8712b8b3f63f845
nimesh <2018cs131@stu.ucsc.cmb.ac.lk>
2021-09-03 21:20:48 +0530

    v2

--- 18E-route-parameters-variables.js
+++ 18E-route-parameters-variables.js
@@ -3,13 +3,13 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/index.html')
+    res.send('this is the home page')
 })
 app.get('/contact', (req, res)=>{
-    res.sendFile(__dirname + '/contact.html')
+    res.send('this is the contact page')
 })
 app.get('/profile/:id', (req, res)=>{
-    res.send(`You requested profile with id of ${req.params.id}`)
+    res.send(`You requested profile with id of ${req.params.id}`) //dynamic string with variable
 })
 
 app.listen(3000)
