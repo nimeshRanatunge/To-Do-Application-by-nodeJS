13682b244c502b47c7871c6bc11d40e9fa929d94
nimesh <2018cs131@stu.ucsc.cmb.ac.lk>
2021-09-03 23:12:28 +0530

    v2

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 app.get('/contact', (req, res)=>{
     res.sendFile(__dirname + '/contact.html')
 })
-app.get('/profile/:id', (req, res)=>{
-    res.send(`You requested profile with id of ${req.params.id}`)
+app.get('/profile/:name', (req, res)=>{
+    res.render('profile', {person: req.params.name})
 })
 
 app.listen(3000)
